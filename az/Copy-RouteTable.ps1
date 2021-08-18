13cd07d6fcbaa26b0341be8ef97b3f4851e2d137
Adam Pavlik <adpavlik@microsoft.com>
2021-08-18 12:57:45 -0400

    added new script to mirror UDR

---
+++ az/Copy-RouteTable.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+    Copy routes to new Route table
+.DESCRIPTION
+    
+#>
+
+param(
+    [Parameter(Mandatory)]
+    [string]$sourceResourceGroup,
+    
+    [Parameter(Mandatory)]
+    [string]$sourceRouteTableName,
+
+    [Parameter(Mandatory)]
+    [string]$destinationResourceGroup,
+
+    [Parameter(Mandatory)]
+    [string]$destinationRouteTableName,
+
+    [Parameter(Mandatory)]
+    [string]$destinationLocation
+)
+
+# get routes from source route table
+$sourceRoutes = (Get-azRoutetable -ResourceGroupName $sourceResourceGroup `
+                                  -Name $sourceRouteTableName).Routes
+
+# check for desitnation routes, throw error if empty
+if (!$sourceRoutes) {
+    Write-Output "Error: no routes found in source Route table"
+    throw
+}
+
+# Create route table
+$newRouteTable = New-AzRouteTable -Name $destinationRouteTableName `
+                                  -ResourceGroupName $destinationResourceGroup `
+                                  -location $destinationLocation
+
+# Populate newly create route table with routes from source
+foreach ($route in $sourceRoutes) {
+    
+    Write-Output "Adding Route Name: $($route.Name) to $destinationRouteTableName"
+
+    Get-AzRouteTable -ResourceGroupName $destinationResourceGroup `
+                     -Name $newRouteTable.Name `
+                     | Add-AzRouteConfig `
+                     -Name $route.Name `
+                     -AddressPrefix $route.AddressPrefix `
+                     -NextHopType $route.NextHopType `
+                     | Set-AzRouteTable `
+                     | Out-Null
+
+}
